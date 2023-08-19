74f40e500ff94882698ad6d5e033517a8aef01ac
r23 <info@r23.de>
2023-08-19 20:37:28 +0200

    Add loglout
    A wish from Webmark in the Forum
    https://foren.myoos.de/viewtopic.php?p=7799#p7799

---
+++ logout.php
@@ -0,0 +1,34 @@
+<?php
+/* ----------------------------------------------------------------------
+
+   MyOOS [Dumper]
+   http://www.oos-shop.de/
+
+   Copyright (c) 2003 - 2023 by the MyOOS Development Team.
+   ----------------------------------------------------------------------
+   Based on:
+
+   MySqlDumper
+   http://www.mysqldumper.de
+
+   Copyright (C)2004-2011 Daniel Schlichtholz (admin@mysqldumper.de)
+   ----------------------------------------------------------------------
+   Released under the GNU General Public License
+   ---------------------------------------------------------------------- */
+
+
+if (isset($_SERVER['HTTPS']) && (strtolower((string) $_SERVER['HTTPS']) == 'on' || $_SERVER['HTTPS'] == 1)) {
+	$scheme = 'https';
+} elseif (isset($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443) {
+	$scheme = 'https';
+} else {
+	$scheme = 'http';
+}
+
+// Set the URL to redirect to
+$logout_url = sprintf('%s://logout:logout@%s%s', $scheme, $_SERVER['HTTP_HOST'], dirname((string) $_SERVER['PHP_SELF']));
+
+
+// Send the headers to redirect the user
+header("Location: $logout_url");
+exit;
